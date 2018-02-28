53e918389a27e974ea56c9811c1ea4b95e6ddd6c
创客金融 <chuangkejinrong@xin.local>
2018-02-28 09:46:56 +0800

    初始化代码仓库

---
+++ AppExtension/AppExtension/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AppExtension
+//
+//  Created by 创客金融 on 2018/2/28.
+//  Copyright © 2018年 创客金融. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ AppExtension/AppExtensionTests/AppExtensionTests.m
@@ -0,0 +1,39 @@
+//
+//  AppExtensionTests.m
+//  AppExtensionTests
+//
+//  Created by 创客金融 on 2018/2/28.
+//  Copyright © 2018年 创客金融. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AppExtensionTests : XCTestCase
+
+@end
+
+@implementation AppExtensionTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
